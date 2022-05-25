39abc2ed2856f40dc772bce5f2065f55ae8efd4c
Henrique Dowsley <henrique.dowsley89@hotmail.com>
2022-05-25 16:42:54 -0300

    Estado de demonstração.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+// import { SQLite } from '@awesome-cordova-plugins/sqlite/ngx';
+import { SQLite } from '@ionic-native/sqlite/ngx';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
@@ -11,7 +13,7 @@ import { AppComponent } from './app.component';
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [SQLite, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,15 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CrudService } from '../services/crud.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
 
-  nameVal: string = "";
+export class Tab2Page implements OnInit {
 
-  constructor() {}
+  nameC: string = '';
+  partyC: string = '';
+  voteInput: string = '';
+  voteList: Array<any>;
+  candidateList: Array<any>;
 
-  limpa() { this.nameVal = ""; }
+  constructor(private crud: CrudService) { }
+
+  ngOnInit() {
+    this.crud.databaseConn();
+  }
+
+  ionViewDidEnter() {  
+    this.voteList = this.crud.getAllVotes();
+    this.candidateList = this.crud.getAllCandidate();
+  }
+
+  clear() {
+    this.voteInput = '';
+  }
+
+  confirm() {
+    this.crud.addVote(this.voteInput);
+    this.voteList = this.crud.getAllVotes();
+    this.clear
+  }
+
+  onChangeInput(){
+    this.nameC = '';
+    this.partyC = '';
+    this.crud.getCandidateInfo(this.voteInput).then((res) => {
+      this.nameC = res['nameCand'];
+      this.partyC = res['partyCand'];
+    })
+  }
 }
